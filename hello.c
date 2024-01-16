e020721aeac69536827441f0726e5127e6472590
elandraaa <2026lee.keira@fcboe.org>
2024-01-16 13:20:56 -0500

    completed mario assignment

--- hello.c
+++ hello.c
@@ -2,6 +2,8 @@
 
 int main()
 {
-	printf("Hello, IST!\n");
+	printf("Hello, elandra! never procrastinate ever again and do more ctfs at home!\n");
+	printf(" #\n");
+	printf("##\n");	
 	return 0;
 }
---
+++ mario.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello, elandra! never procrastinate ever again and do more ctfs at home!\n");
+	printf("        #\n");
+	printf("       ##\n");
+	printf("      ###\n");
+	printf("     ####\n");
+	printf("    #####\n");
+	printf("   ######\n");
+	printf("  #######\n");
+	printf(" ########\n");
+	return 0;
+}
